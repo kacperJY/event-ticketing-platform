807d52f2e8d2bcf47a9c047861ca0d5fa8bd4b3d
Kacper <kacper.junoryy@gmail.com>
2026-07-08 21:01:06 +0200

    chore: initialize monorepo with Spring Boot service skeletons

---
+++ fulfillment-worker/src/main/java/pl/kacper/fulfillment_worker/FulfillmentWorkerApplication.java
@@ -0,0 +1,13 @@
+package pl.kacper.fulfillment_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillmentWorkerApplication.class, args);
+	}
+
+}
---
+++ fulfillment-worker/src/test/java/pl/kacper/fulfillment_worker/FulfillmentWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kacper.fulfillment_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillmentWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
